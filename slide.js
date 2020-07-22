30a0127f9f3600212631a5cca0c7821981111a93
ducda26 <63221856+ducda26@users.noreply.github.com>
2020-07-23 00:16:49 +0700

    // Bỏ tất cả màu đèn trước khi add

---
+++ slide.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded",function () {
+	// Khai báo một số đối tượng cần sử dụng
+    var nut = document.querySelectorAll('.chuyenslide ul li');
+    // nút trả về một mảng
+
+    // bắt sự kiện click cho từng nút
+    for (var i = 0; i < nut.length; i++) {
+		nut[i].addEventListener('click',function () {
+            // Bỏ tất cả màu đèn trước khi add
+			for (var i = 0; i < nut.length; i++) {
+				nut[i].classList.remove('kichhoat');
+			}
+			this.classList.add('kichhoat');
+			// Hết phần chuyển màu nút Slide
+        }
