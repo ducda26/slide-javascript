402b15c0fe4f2259a888d4640c69c2cdd3917527
ducda26 <63221856+ducda26@users.noreply.github.com>
2020-07-23 00:18:39 +0700

    Phan chuyen mau nut slide

--- slide.js
+++ slide.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded",function () {
 	// Khai báo một số đối tượng cần sử dụng
     var nut = document.querySelectorAll('.chuyenslide ul li');
     // nút trả về một mảng
-
+      
     // bắt sự kiện click cho từng nút
     for (var i = 0; i < nut.length; i++) {
 		nut[i].addEventListener('click',function () {
